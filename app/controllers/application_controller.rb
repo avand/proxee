6bb7b8b33b99078856a4a4f571dc0b81b4861f5d
Avand Amiri <avand@avandamiri.com>
2016-08-16 21:12:50 -0700

    Allowing cross domain requests for all requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  before_filter :allow_cross_origin_requests
+
+protected
+
+  def allow_cross_origin_requests
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,7 +3,6 @@ class RequestsController < ApplicationController
   def get
     remote_response = HTTParty.get(params[:url])
 
-    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Content-Type'] = remote_response.content_type
 
     render text: remote_response.body
